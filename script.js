a5464668eae7536c289f329784d7f1ded42b226a
Krithik M <112496915+Krithik-m-codes@users.noreply.github.com>
2023-11-09 14:50:19 +0000

    Added footer copyright section and fixed issuses

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const buttons = document.querySelectorAll('button');
-console.log(buttons);
+// function toggle_menu(){
+//     const toggle_menu = document.querySelector('.menu-toggle');
+//     const navigation = document.querySelector('.navigation');
+//     toggle_menu.classList.toggle('active');
+//     navigation.classList.toggle('active');
+//     menuOpen = !menuOpen;
+//     console.log(menuOpen);
+
+    
+// }
+
+// let menuOpen = false;
+
