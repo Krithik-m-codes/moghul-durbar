144aabce8c07b5dbbe00c031aa30641f0ff65fb1
Krithik M <112496915+Krithik-m-codes@users.noreply.github.com>
2023-11-10 13:00:15 +0000

    resolved logo issues

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// function toggle_menu(){
-//     const toggle_menu = document.querySelector('.menu-toggle');
-//     const navigation = document.querySelector('.navigation');
-//     toggle_menu.classList.toggle('active');
-//     navigation.classList.toggle('active');
-//     menuOpen = !menuOpen;
-//     console.log(menuOpen);
-
-    
-// }
-
-// let menuOpen = false;
-
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+hamburgerMenu.addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change");fig
+    });
