752cf0084ed975860fcd267635d6ddddf7bee935
Krithik M <112496915+Krithik-m-codes@users.noreply.github.com>
2023-11-28 17:48:20 +0000

    redesigned about us esction and optimised code

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+
 const mobileMenu = document.getElementById('mobile-menu');
 const mobileMenuBtn = document.querySelector('.btn-mobile-nav'); // Fixed the selector
+const mobileMenuCloseBtn = document.querySelector('.btn-mobile-nav-close'); // Fixed the selector
 
-console.log(mobileMenuBtn);
 mobileMenuBtn.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-});
+    mobileMenu.classList.remove('hidden');
+}   
+);
+
+
+mobileMenuCloseBtn.addEventListener('click', () => {
+    mobileMenu.classList.add('hidden');
+}
+);
+
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+        mobileMenu.classList.add('hidden');
+    }
+}  );
+
