2031f7e618120e2a22cb71030df42df94c5ef213
Krithik M <112496915+Krithik-m-codes@users.noreply.github.com>
2024-01-07 17:49:47 +0000

    fixed and optimised responsiveness

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,34 @@
+// For the header to hide when scrolling down and show when scrolling up
+document.body.addEventListener("scroll", () => {
+  // console.log('scrolling')
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("main-header").style.top = "-100px";
+  } else {
+    document.getElementById("main-header").style.top = "0px";
+  }
+});
 
-// For the header to hide when scrolling down and show when scrolling up 
-document.body.addEventListener('scroll', () => {
-    // console.log('scrolling')
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        document.getElementById("main-header").style.top = "-100px";
-    } else {
-        document.getElementById("main-header").style.top = "0px";
-    }
-})
-
-
-
-
-// for mobile devices menu open / close 
-const hamBtn = document.querySelector('.hamburger-menu');
+// for mobile devices menu open / close
+const hamBtn = document.querySelector(".hamburger-menu");
 
 function hamburgerHandler(e) {
-    // e.preventDefault();
-    e.classList.toggle('change');
+  if (e.classList.contains("change")) {
+    e.classList.remove("change");
+    document.getElementById("mobileNav").style.width = "0%";
+  } else {
+    e.classList.add("change");
+    document.getElementById("mobileNav").style.width = "100%";
+  }
 }
 
-// hamBtn.addEventListener('click', (e) => {
-//     if (e.target.classList.contains('change')) {
-//         e.target.classList.remove('change');
-//     }
-//     else {
-//         e.target.classList.add('change');
-//     }
-//     console.log('clicked')
-//     // const nav = document.querySelector('.mobNav-links');
-//     // nav.classList.toggle('open');
-// }
-// )
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("mobileNav").style.width = "0%";
+  hamBtn.classList.remove("change");
+}
 
-
-
+// Window on load function
 window.onload = () => {
-    console.log('loaded')
-    document.getElementById("main-header").style.top = "0";
-}
-
-
+  console.log("loaded");
+  document.getElementById("main-header").style.top = "0";
+};
